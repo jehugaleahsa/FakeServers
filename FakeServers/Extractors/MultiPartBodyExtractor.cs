0fa9607f6d9c6875181878df0e31c69ab765b42b
Travis Parks <jehugaleahsa@gmail.com>
2015-10-27 19:18:10 -0400

    MultiPartExtractor (WIP)

---
+++ FakeServers/Extractors/MultiPartBodyExtractor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net;
+
+namespace FakeServers.Extractors
+{
+    public class MultiPartBodyExtractor : IRequestBodyExtractor
+    {
+        private readonly List<MultiPartFile> files;
+
+        public MultiPartBodyExtractor()
+        {
+            this.files = new List<MultiPartFile>();
+        }
+
+        public NameValueCollection Parameters { get; private set; }
+
+        public IEnumerable<MultiPartFile> Files
+        {
+            get { return files; }
+        }
+
+        public void Extract(HttpListenerRequest request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException("request");
+            }
+            if (!request.HasEntityBody)
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ FakeServers/Extractors/MultiPartFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FakeServers.Extractors
+{
+    public class MultiPartFile
+    {
+        public string FileName { get; private set; }
+
+        public byte[] Contents { get; private set; }
+
+        public string ContentType { get; private set; }
+    }
+}
