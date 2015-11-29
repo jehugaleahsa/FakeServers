efedbcdc915a6ea1223ea2eceb2bb24ee61b2adf
jehugaleahsa <jehugaleahsa@gmail.com>
2015-11-29 12:02:30 -0500

    Create JsonBodyExtractor

---
+++ FakeServers/Extractors/JsonBodyExtractor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace FakeServers.Extractors
+{
+    public class JsonBodyExtractor<T> : IRequestBodyExtractor
+    {
+        private readonly JsonSerializerSettings settings;
+
+        public JsonBodyExtractor()
+            : this(new JsonSerializerSettings())
+        {
+        }
+
+        public JsonBodyExtractor(JsonSerializerSettings settings)
+        {
+            this.settings = settings;
+        }
+
+        public T Result { get; private set; }
+
+        public void Extract(HttpListenerRequest request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException("request");
+            }
+            if (!request.HasEntityBody)
+            {
+                return;
+            }
+            using (StreamReader reader = new StreamReader(request.InputStream))
+            {
+                string content = reader.ReadToEnd();
+                Result = JsonConvert.DeserializeObject<T>(content, settings);
+            }
+        }
+    }
+}
